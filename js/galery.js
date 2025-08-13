a7661aadd7bb4e00118ee0a40f078534d9c5b81b
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2025-08-13 14:08:07 +0300

    feat: add googl view

---
+++ js/galery.js
@@ -0,0 +1,16 @@
+export default randomGalary();
+
+
+
+function randomGalary() {
+    const pictureGalary = document.querySelector(".gallery-img");
+    let arr = ['./assets/img/galery/galery1.jpg', './assets/img/galery/galery2.jpg', './assets/img/galery/galery3.jpg', './assets/img/galery/galery4.jpg', './assets/img/galery/galery5.jpg', './assets/img/galery/galery6.jpg', './assets/img/galery/galery7.jpg', './assets/img/galery/galery8.jpg', './assets/img/galery/galery9.jpg', './assets/img/galery/galery10.jpg', './assets/img/galery/galery11.jpg', './assets/img/galery/galery12.jpg', './assets/img/galery/galery13.jpg', './assets/img/galery/galery14.jpg', './assets/img/galery/galery15.jpg'];
+    arr.sort(() => Math.random() - 0.5);
+    arr.forEach(function (item) {
+        const img = document.createElement('img');
+        img.src = item;
+        img.alt = "gallery img";
+        console.log(img);
+        pictureGalary.appendChild(img);
+    });
+};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import exploreImgChange from './explore.js';
 import videoPlayer from './video.js';
+import randomGalary from './galery.js';
 
 
 // Свайпер Welcome
@@ -57,22 +58,13 @@ function updateFraction(swiperInstance) {
     document.querySelector('.fraction-pagination').textContent = `${current} / ${total}`;
 }
 
-const pictureGalary = document.querySelector(".gallery-img");
-let arr = ['./assets/img/galery/galery1.jpg', './assets/img/galery/galery2.jpg', './assets/img/galery/galery3.jpg', './assets/img/galery/galery4.jpg', './assets/img/galery/galery5.jpg', './assets/img/galery/galery6.jpg', './assets/img/galery/galery7.jpg', './assets/img/galery/galery8.jpg', './assets/img/galery/galery9.jpg', './assets/img/galery/galery10.jpg', './assets/img/galery/galery11.jpg', './assets/img/galery/galery12.jpg', './assets/img/galery/galery13.jpg', './assets/img/galery/galery14.jpg', './assets/img/galery/galery15.jpg'];
-arr.sort(() => Math.random() - 0.5);
-arr.forEach(function (item) {
-    const img = document.createElement('img');
-    img.src = item;
-    img.alt = "gallery img";
-    console.log(img);
-    pictureGalary.appendChild(img);
-});
-// const img = document.createElement("img");
-// img.src = `./assets/img/galery/galery1.jpg`;
-// img.alt = `galery1`;
-// pictureGalary.append(img);
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     exploreImgChange();
     videoPlayer();
+    randomGalary();
 });
